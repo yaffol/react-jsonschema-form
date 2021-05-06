631f78539c35efd991ec43a1cf0d5ffe68dec798
Patrick Vale <customintelligence@gmail.com>
2021-05-06 19:28:43 +0100

    Changes add button colour to blue (secondary)

--- packages/material-ui/src/AddButton/AddButton.tsx
+++ packages/material-ui/src/AddButton/AddButton.tsx
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
 const AddButton: React.FC<AddButtonProps> = props => (
-  <Button {...props} color="secondary">
+  <Button {...props} color="primary">
     <AddIcon /> Add Item
   </Button>
 );
