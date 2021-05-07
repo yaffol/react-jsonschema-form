76d554edd062a0fec4c07b8146b17ba2a0f5b064
Patrick Vale <customintelligence@gmail.com>
2021-05-07 06:02:52 +0100

    makes paths relative

--- update_manifest.sh
+++ update_manifest.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-cd /app/packages/playground/src
+cd ./packages/playground/src
 node translate.js
-cd /app/packages/playground/src/data
+cd data
 python3 generate-manifest.py
-cd /app/packages/playground
