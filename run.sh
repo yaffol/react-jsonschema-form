f6b0e1c0ebe6f5915fa6136dc48063c8127ad5f2
Patrick Vale <customintelligence@gmail.com>
2021-05-10 15:56:47 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ DOCKER_IMAGE=form-runner
 DOCKER_IMAGE_TAG=latest
 
 ./check-env.sh
-echo "Return value of check-env.sh: ${check_env_code}"
+check_env_code=$?
+echo "Return value from check-env.sh: ${check_env_code}"
 if [ ! $check_env_code -eq 0 ]
 then
   echo "Cancelled by user... Exiting."
@@ -11,4 +12,3 @@ then
 fi
 docker image inspect form-runner:latest >/dev/null 2>&1 && echo 'Form runner image exists... Starting container...' || ./build.sh
 docker run -v "$(pwd)/data":/app/packages/playground/src/data --mount type=bind,source="${GOOGLE_APPLICATION_CREDENTIALS}",target=/creds/gcsa.json -p 8080:8080 form-runner:latest
-
