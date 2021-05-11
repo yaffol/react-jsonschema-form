eb874afbd41a56afb43ee95688a34adc0ed5c6f5
Patrick Vale <customintelligence@gmail.com>
2021-05-11 23:25:45 +0100

    Adds build dir, and option to build instead of live serve

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,14 @@ umask 0000
 
 ./update_manifest.sh
 cd /app/packages/playground
-npm run start
+if [ -z ${1+x} ]
+then echo "arg1 is unset"
+  npm run start
+else
+  echo "arg1 is set to '$1'"
+  # SHOW_NETLIFY_BADGE causes the playground build process to use
+  # an empty webpack publicPath
+  export SHOW_NETLIFY_BADGE=1
+  npm run build
+  cp -av build "/app/build/$1"
+fi
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ source config.sh
 
 echo "Looking for docker image: ${IMG}"
 docker image inspect $IMG  >/dev/null 2>&1 && echo 'Form runner image exists... Starting container...' || ./build.sh
-docker run -v "$(pwd)/data":/app/packages/playground/src/data --mount type=bind,source="${GOOGLE_APPLICATION_CREDENTIALS}",target=/creds/gcsa.json -p 8080:8080 $IMG
+docker run -v "$(pwd)/data":/app/packages/playground/src/data --mount type=bind,source="$(pwd)/build",target=/app/build  --mount type=bind,source="${GOOGLE_APPLICATION_CREDENTIALS}",target=/creds/gcsa.json -p 8080:8080 $IMG "$@"
