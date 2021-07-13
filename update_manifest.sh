739bbcdd2b14a8a58e6c15bb458b872118a70018
Patrick Vale <pvale@crossref.org>
2021-07-13 06:45:18 +0100

    suppress pushd output

--- update_manifest.sh
+++ update_manifest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ### Update the Form Runner's manifest.json ###
 
-pushd ./packages/playground/src
+pushd ./packages/playground/src > /dev/null
 node translate.js
 python3 generate-manifest.py
-popd
+popd > /dev/null
